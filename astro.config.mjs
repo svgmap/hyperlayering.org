6b3567f98cfd66408f8cc14830717d34de099a6f
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2026-06-23 19:25:04 -0400

    fix: standardize quotes in astro.config.mjs for base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
 	// Temporary site + base config for github pages deployment
 	site: "https://svgmap.github.io", // TODO: Change when migrated to real domain
-	base: 'hyperlayering.org',
+	base: "hyperlayering.org",
 	integrations: [svelte()],
 	i18n: {
 		locales: ["en", "ja"],
