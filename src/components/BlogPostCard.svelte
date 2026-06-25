c56267cb89a06120bfd649e188808a1742b8de93
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2026-06-25 00:59:04 -0400

    feat: add BlogPostCard component and update blog index page structure

---
+++ src/components/BlogPostCard.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  interface BlogPostCardProps {
+    title: string;
+    author: string;
+    date: string;
+    url: string;
+  }
+
+  let { title, author, date, url }: BlogPostCardProps = $props();
+</script>
+
+<a href={url} aria-label="">
+  <article class="blog-post--card">
+    <h2 aria-labelledby="" class="post-title">{title}</h2>
+    <span class="post-author">{author}</span>
+    <span class="post-date">{date}</span>
+  </article>
+</a>
+
+<style scoped>
+  .blog-post-card {
+    
+  }
+
+  .card-wrapper {
+
+  }
+</style>
