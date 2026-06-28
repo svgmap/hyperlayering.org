8cb6f14ed885764ec65da8822d78039a22e22ef3
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2026-06-27 22:31:28 -0500

    feat: update BlogPostCard component and restructure blog pages

--- src/components/BlogPostCard.svelte
+++ src/components/BlogPostCard.svelte
@@ -2,27 +2,27 @@
   interface BlogPostCardProps {
     title: string;
     author: string;
-    date: string;
+    date: Date;
     url: string;
   }
 
   let { title, author, date, url }: BlogPostCardProps = $props();
 </script>
 
-<a href={url} aria-label="">
-  <article class="blog-post--card">
-    <h2 aria-labelledby="" class="post-title">{title}</h2>
+<a href={url} aria-label="" class="card-wrapper" clickable="true">
+  <article class="blog-post-card card stack">
+    <h2 aria-labelledby="" class="title">{title}</h2>
     <span class="post-author">{author}</span>
-    <span class="post-date">{date}</span>
+    <span class="post-date">{date.toLocaleString("en-GB", { dateStyle: "short" })}</span>
   </article>
 </a>
 
 <style scoped>
   .blog-post-card {
-    
+    text-decoration: none;
   }
 
   .card-wrapper {
-
+    text-decoration: none;
   }
 </style>
