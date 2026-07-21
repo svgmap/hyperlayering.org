defbd84b95dfb0c6df555d942b0d70f883b9f899
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2026-07-21 14:37:51 -0400

    Add Logo component with customizable size and color

---
+++ src/components/Icons/Logo.svelte
@@ -0,0 +1,89 @@
+<script lang="ts">
+let { size = 48, color = "currentColor", className = "" }: IconProps = $props();
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  xml:space="preserve"
+  width={size}
+  height={size}
+  version="1.1"
+  style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
+  viewBox="0 0 1000 1000"
+  xmlns:xlink="http://www.w3.org/1999/xlink"
+  xmlns:xodm="http://www.corel.com/coreldraw/odm/2003"
+>
+  <g id="Layer_x0020_1">
+    <g id="_1620321422736">
+      <path
+        class="fil0 str0"
+        d="M662.35 155.43c-24.55,-12.31 -50.69,-21.93 -78.04,-28.44m-436.54 436.54c6.5,27.35 16.12,53.49 28.44,78.04m165.77 163.71c24.85,12.07 51.28,21.4 78.91,27.57m432.74 -432.74c-6.17,-27.63 -15.49,-54.06 -27.57,-78.91"
+      />
+      <polyline
+        class="fil0 str0"
+        points="344.26,367.05 629.63,367.05 629.63,442.33 375.45,442.33 "
+      />
+      <path
+        class="fil0 str0"
+        d="M375.45 442.33l-199.24 199.24m-28.44 -78.04l196.49 -196.49m75.29 -75.29l40.26 -40.26"
+      />
+      <polyline
+        class="fil0 str0"
+        points="660.82,592.91 395.45,592.91 395.45,517.63 629.63,517.63 "
+      />
+      <path
+        class="fil0 str0"
+        d="M629.63 517.63l196.43 -196.43m27.57 78.91l-192.81 192.81m-75.29 75.29l-40.26 40.26"
+      />
+      <path
+        class="fil0 str0"
+        d="M395.45 592.91l265.37 0m-75.29 75.29l-106.47 0"
+      />
+      <path
+        class="fil0 str0"
+        d="M419.55 291.76l106.47 0m103.61 75.29l-285.37 0"
+      />
+      <path
+        class="fil0 str0"
+        d="M419.55 291.76l164.77 -164.77m78.04 28.44l-136.33 136.33"
+      />
+      <path
+        class="fil0 str0"
+        d="M585.53 668.2l-164.65 164.65m-78.91 -27.57l137.08 -137.08"
+      />
+    </g>
+    <path
+      class="fil1 str1"
+      d="M375.45 442.33l254.19 0 0 -75.29 -285.37 0 -196.49 196.49c6.5,27.35 16.12,53.49 28.44,78.04l199.24 -199.24z"
+    />
+    <path
+      class="fil1 str1"
+      d="M419.55 291.76l106.47 0 136.33 -136.33c-24.55,-12.31 -50.69,-21.93 -78.04,-28.44l-124.51 124.51 -40.26 40.26z"
+    />
+    <path
+      class="fil1 str1"
+      d="M826.06 321.2l-196.43 196.43 -234.19 0 0 75.29 265.37 0 192.81 -192.81c-6.17,-27.63 -15.49,-54.06 -27.57,-78.91z"
+    />
+    <path
+      class="fil1 str1"
+      d="M585.53 668.2l-106.47 0 -137.08 137.08c24.85,12.07 51.28,21.4 78.91,27.57l124.39 -124.39 40.26 -40.26z"
+    />
+  </g>
+</svg>
+
+<style>
+  .str0, .str1 {
+    stroke: currentColor;
+    stroke-width: 6.94;
+    stroke-linecap: round;
+    stroke-linejoin: round;
+    stroke-miterlimit: 2.61313;
+  }
+
+  .fil0 {
+    fill: none;
+  }
+  .fil1 {
+    fill: currentColor;
+  }
+</style>
