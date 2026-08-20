196a7c850948bb0e22cd31a8a0a2f5fba21e52d4
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2026-08-20 13:32:56 -0400

    Update UpdateCard component to include description and adjust layout in Updates page

--- src/components/UpdateCard.svelte
+++ src/components/UpdateCard.svelte
@@ -2,40 +2,63 @@
 interface UpdateCardProps {
 	title: string;
 	author: string;
+  description: string;
 	date: Date;
 	url: string;
 }
 
-let { title, author, date, url }: UpdateCardProps = $props();
+let { title, author, description, date, url }: UpdateCardProps = $props();
 </script>
 
 <a href={url} aria-label="" class="post-listing" clickable="true">
   <article class="stack">
-    <span aria-labelledby="" class="title">{title}</span>
-    <hr>
-    <span class="post-author">{author}</span>
-    <span class="post-date">{date.toLocaleString("en-GB", { dateStyle: "short" })}</span>
+    <h2 aria-labelledby="" class="title">{title}</h2>
+    <p class="description">{description}</p>
+    <div class="row details">
+      <span class="post-author">{author}</span><span>&#x2022</span>
+      <span class="post-date">{date.toLocaleString("en", { dateStyle: "long" })}</span>
+
+    </div>
   </article>
 </a>
 
 <style scoped>
 
   .post-listing {
-    color: var(--text-secondary);
+    color: var(--text-primary);
     text-decoration: none;
     padding: var(--space-sm);
     border-radius: var(--round-md);
     transition: background-color var(--timing-normal) ease-out;
 
     .title {
+      margin: 0;
       font-size: var(--font-xl);
+      font-width: var(--font-wide);
+      font-stretch: var(--font-wide);
       font-weight: bold;
+      text-decoration-color: transparent;
+      text-underline-offset: var(--space-sm);
+      transition:
+        color var(--timing-fast),
+        text-decoration-color var(--timing-fast),
+        text-underline-offset var(--timing-fast);
+    }
+    
+    .description {
+      color: var(--text-secondary);
+    }
+
+    .details {
+      color: var(--text-tertiary);
+      font-size: var(--font-sm);
     }
 
     &:hover {
       background-color: var(--bg-secondary);
       .title {
-        text-decoration: underline;
+        text-decoration: underline solid var(--accent) 2px;
+        text-underline-offset: var(--space-xs);
       }
     }
   }
