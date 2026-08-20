7ebbb49b7c79b9da247d47597a364960be3a2d98
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2026-08-20 18:18:28 -0400

    Add RightArrow component as an icon for navigation

---
+++ src/components/Icons/RightArrow.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+let { size = 24, color = "currentColor", className = "" }: IconProps = $props();
+</script>
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width={size}
+  height={size}
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke={color}
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  class={`lucide lucide-arrow-right-icon lucide-arrow-right ${className}`}
+  ><path d="M5 12h14" /><path d="m12 5 7 7-7 7" /></svg
+>
