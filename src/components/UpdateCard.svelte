3e1ee187a4853d7ffc4896ea0fb330ffb3638592
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2026-08-24 19:12:39 -0400

    Add language prop to UpdateCard for localization support

--- src/components/UpdateCard.svelte
+++ src/components/UpdateCard.svelte
@@ -1,15 +1,23 @@
 <script lang="ts">
   import RightArrow from "./Icons/RightArrow.svelte";
 
-interface UpdateCardProps {
-	title: string;
-	author: string;
-  description: string;
-	date: Date;
-	url: string;
-}
+  interface UpdateCardProps {
+    title: string;
+    author: string;
+    description: string;
+    date: Date;
+    url: string;
+    lang: string;
+  }
 
-let { title, author, description, date, url }: UpdateCardProps = $props();
+  let {
+    title,
+    author,
+    description,
+    date,
+    url,
+    lang = "en",
+  }: UpdateCardProps = $props();
 </script>
 
 <a href={url} aria-label="" class="post-listing" clickable="true">
@@ -18,14 +26,15 @@ let { title, author, description, date, url }: UpdateCardProps = $props();
     <p class="description">{description}</p>
     <div class="row details">
       <span class="post-author">{author}</span><span>&#x2022</span>
-      <span class="post-date">{date.toLocaleString("en", { dateStyle: "long" })}</span>
-      <RightArrow size="1rem" className="details-arrow"/>
+      <span class="post-date"
+        >{date.toLocaleString(lang, { dateStyle: "long" })}</span
+      >
+      <RightArrow size="1rem" className="details-arrow" />
     </div>
   </article>
 </a>
 
 <style>
-
   .post-listing {
     color: var(--text-primary);
     text-decoration: none;
@@ -46,7 +55,7 @@ let { title, author, description, date, url }: UpdateCardProps = $props();
         text-decoration-color var(--timing-fast),
         text-underline-offset var(--timing-fast);
     }
-    
+
     .description {
       color: var(--text-secondary);
     }
@@ -54,11 +63,6 @@ let { title, author, description, date, url }: UpdateCardProps = $props();
     .details {
       color: var(--text-tertiary);
       font-size: var(--font-sm);
-    }
-
-    .details-arrow {
-      visibility: none;
-      display: none;
     }
 
     &:hover {
@@ -69,5 +73,4 @@ let { title, author, description, date, url }: UpdateCardProps = $props();
       }
     }
   }
-  
 </style>
