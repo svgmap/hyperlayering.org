4ce7f17e3fee2a7220ebd19223f3b81d97199f31
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2026-08-28 15:29:29 -0400

    Adding hover/focus animation for the arrow element

--- src/components/UpdateCard.svelte
+++ src/components/UpdateCard.svelte
@@ -18,21 +18,35 @@ let {
 	url,
 	lang = "en",
 }: UpdateCardProps = $props();
+
+let updateLinkElement: HTMLAnchorElement | null = $state(null);
 </script>
 
-<a href={url} aria-label="" class="post-listing" clickable="true">
-  <article class="stack">
-    <h2 aria-labelledby="" class="title">{title}</h2>
-    <p class="description">{description}</p>
-    <div class="row details">
-      <span class="post-author">{author}</span><span>&#x2022</span>
-      <span class="post-date"
-        >{date.toLocaleString(lang, { dateStyle: "long" })}</span
-      >
-      <RightArrow size="1rem" className="details-arrow" />
-    </div>
-  </article>
-</a>
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+<article
+  class="post-listing stack"
+  onclick={(e: MouseEvent) => {
+    console.log("test")
+    if(updateLinkElement && e.target !== updateLinkElement) {
+      
+      updateLinkElement.click();
+    }
+  }}
+>
+  <a bind:this={updateLinkElement} href={url} class="link-unstyled"
+    ><h2 class="title">{title}</h2></a
+  >
+  <p class="description">{description}</p>
+  <div class="row details">
+    <p class="post-author">{author}</p>
+    <span>&#x2022</span>
+    <p class="post-date">
+      {date.toLocaleString(lang, { dateStyle: "long" })}
+    </p>
+    <RightArrow size="1rem" className="details-arrow" />
+  </div>
+</article>
 
 <style>
   .post-listing {
@@ -41,6 +55,7 @@ let {
     padding: var(--space-sm);
     border-radius: var(--round-md);
     transition: background-color var(--timing-normal) ease-out;
+    cursor: pointer;
 
     .title {
       margin: 0;
@@ -65,11 +80,29 @@ let {
       font-size: var(--font-sm);
     }
 
-    &:hover {
+    :global(.details-arrow) {
+      transition-property: opacity, display, transform;
+      transition-duration: var(--timing-fast);
+      transition-behavior: allow-discrete;
+      display: none;
+      opacity: 0;
+    }
+
+    &:hover, &:focus-within {
       background-color: var(--bg-secondary);
       .title {
         text-decoration: underline solid var(--accent) 2px;
         text-underline-offset: var(--space-xs);
+      }
+      :global(.details-arrow) {
+        display: block;
+        opacity: 1;
+        transform: translateX(0);
+
+        @starting-style {
+          opacity: 0;
+          transform: translateX(-25%);
+        }
       }
     }
   }
