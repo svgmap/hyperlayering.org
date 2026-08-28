b546a0204da13eeffcbbf5d5e9b2414d97d1436e
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2026-08-28 15:38:51 -0400

    Reformatting update card to use enhanced base card class styling

--- src/components/UpdateCard.svelte
+++ src/components/UpdateCard.svelte
@@ -25,19 +25,17 @@ let updateLinkElement: HTMLAnchorElement | null = $state(null);
 <!-- svelte-ignore a11y_click_events_have_key_events -->
 <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
 <article
-  class="post-listing stack"
+  class="post-listing card stack"
+  clickable="true"
   onclick={(e: MouseEvent) => {
-    console.log("test")
-    if(updateLinkElement && e.target !== updateLinkElement) {
-      
+    if (updateLinkElement && e.target !== updateLinkElement)
       updateLinkElement.click();
-    }
   }}
 >
   <a bind:this={updateLinkElement} href={url} class="link-unstyled"
     ><h2 class="title">{title}</h2></a
   >
-  <p class="description">{description}</p>
+  <p class="body">{description}</p>
   <div class="row details">
     <p class="post-author">{author}</p>
     <span>&#x2022</span>
@@ -52,32 +50,18 @@ let updateLinkElement: HTMLAnchorElement | null = $state(null);
   .post-listing {
     color: var(--text-primary);
     text-decoration: none;
-    padding: var(--space-sm);
-    border-radius: var(--round-md);
-    transition: background-color var(--timing-normal) ease-out;
-    cursor: pointer;
 
     .title {
       margin: 0;
       font-size: var(--font-xl);
       font-width: var(--font-wide);
       font-stretch: var(--font-wide);
-      font-weight: bold;
       text-decoration-color: transparent;
       text-underline-offset: var(--space-sm);
       transition:
         color var(--timing-fast),
         text-decoration-color var(--timing-fast),
         text-underline-offset var(--timing-fast);
-    }
-
-    .description {
-      color: var(--text-secondary);
-    }
-
-    .details {
-      color: var(--text-tertiary);
-      font-size: var(--font-sm);
     }
 
     :global(.details-arrow) {
@@ -88,8 +72,8 @@ let updateLinkElement: HTMLAnchorElement | null = $state(null);
       opacity: 0;
     }
 
-    &:hover, &:focus-within {
-      background-color: var(--bg-secondary);
+    &:hover,
+    &:focus-within {
       .title {
         text-decoration: underline solid var(--accent) 2px;
         text-underline-offset: var(--space-xs);
