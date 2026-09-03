4b0982c2e31f1119a99f724b8160195c9b74762a
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2026-09-03 14:16:00 -0400

    Enhanced Footer Design (#58)
    * Formatting vite config file
    
    * Adding UI strings for new footer elements
    
    * Format indentation in vite.config.js for consistency
    
    * Updating "guidelines" footer section to more general "resources"
    
    * Enhance footer with documentation and resources links
    
    * Enhance main layout and grid responsiveness with CSS adjustments
    
    * Refined footer component with anchor links to secondary pages/resources
    
    * Adding icon for external links (flagged by .external class)
    
    * Add external link support in footer component
    
    * Enhance footer links with external flag for documentation resources
    
    * Reformatting

--- src/components/Footer/Footer.svelte
+++ src/components/Footer/Footer.svelte
@@ -1,16 +1,78 @@
-<footer class="site-footer">
+<script lang="ts">
+import { getLangFromUrl, useTranslations } from "@i18n/utils";
+
+type LinkGroup = {
+	groupLabel: string;
+	links: {
+		label: string;
+		href: string;
+		external?: boolean;
+	}[];
+};
+
+interface FooterProps {
+	linkGroups: LinkGroup[];
+	currentUrl: URL;
+}
+
+let { currentUrl, linkGroups }: FooterProps = $props();
+
+let lang = $derived(getLangFromUrl(currentUrl));
+let t = $derived(useTranslations(lang));
+</script>
+
+<footer class="site-footer stack stack--sm">
+    <section class="link-grid">
+        {#each linkGroups as linkGroup}
+            <article>
+            <h2 class="section-header">{linkGroup.groupLabel}</h2>
+            <ul role="list">
+            {#each linkGroup.links as link}
+                <li><a href={link.href} class={[link.external && "external"]}>{link.label}</a></li>        
+            {/each}
+            </ul>
+        </article>
+        {/each}
+    </section>
+    <hr/>
     <small class="copyright">&#169; 2026 <span translate="no">SVGMap Organization</span></small>
 </footer>
 
 <style>
     .site-footer {
+        margin: var(--space-sm) auto;
+	    margin-inline: var(--space-md);
         background-color: var(--bg-secondary);
         padding: var(--space-md);
-        display: flex;
+        border-radius: var(--round-md);
 
         .copyright {
             margin: 0; 
             color: var(--text-tertiary);
         }
     }
+
+    .link-grid {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+        gap: var(--space-md);
+
+        .section-header {
+            font-size: var(--font-base);
+            margin: 0;
+        }
+
+        a {
+            color: var(--text-tertiary);
+            font-size: var(--font-sm);
+            &:hover {
+                color: var(--text-secondary);
+            }
+        }
+
+        ul {
+            padding-inline-start: 0;
+        }
+    }
+
 </style>
